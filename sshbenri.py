ce15ff257ed0494f0503a49f5edb243d4cb0f7c2
takada-at <takada-at@klab.com>
2013-04-23 14:10:51 +0900

    create

---
+++ sshbenri.py
@@ -0,0 +1,91 @@
+#!/usr/bin/env python
+# coding:utf-8
+
+import json
+from optparse import OptionParser
+import os
+import sys
+    
+def loadconfig(path=None):
+    if path is None:
+        path = os.path.expanduser('~/.projectconfig.py')
+
+    G = {}
+    L = {}
+    execfile(path, G, L)
+    return L['hosts']
+
+def parsecsv(string):
+    if not string: return []
+    return [val.lstrip().rstrip() for val in string.split(',')]
+
+def createssh(hosts, common_options, confpath=None, command=None):
+    commands = []
+    config = loadconfig(confpath)
+    for host in hosts:
+        # sshコマンドをつくる
+        appconfig = config.get(host)
+        if appconfig:
+            expandedhosts = parsecsv(appconfig['host'])
+            sshcommand = createssh(expandedhosts, common_options, confpath)
+            commands.append(sshcommand)
+            continue
+
+        if host.find('ssh ')==0:
+            #もともとSSHコマンドの形をしてる場合はそのまま実行
+            commands.append(host)
+            continue
+
+        port = 22
+        if host.find(':')>=0:
+            host, port = host.split(':')
+            port = int(port)
+
+        sshcommand = "ssh {host} ".format(host=host)
+        if port != 22:
+            sshcommand += "-p {port} ".format(port=port)
+
+        if common_options:
+            # 全部につけるオプション
+            sshcommand += '%s ' % (' '.join(common_options),)
+
+        commands.append(sshcommand)
+
+    if command:
+        commands.append(command)
+
+    executecommand = " ".join(commands)
+    return executecommand
+
+def executessh(hosts, common_options, execcmd, confpath=None):
+    executecommand = createssh(hosts, common_options, confpath, command=execcmd)
+    print executecommand
+    os.system(executecommand)
+
+def _create_forwardopt(ports):
+    res = []
+    for fport in ports:
+        res.append("-L{port}:localhost:{port}".format(port=fport))
+
+    return res
+
+def main():
+    usage = "usage: %prog [options] HOST[,HOST..]"
+    parser = OptionParser(usage=usage)
+    parser.add_option('-p', '--ports', dest='ports', help='forward port')
+    parser.add_option('-e', '--exec', dest='execcmd', help='execute comand')
+    opt, args = parser.parse_args()
+    if len(args) < 1:
+        parser.print_help()
+        exit()
+
+    hosts = parsecsv(args[0])
+    ports = parsecsv(opt.ports)
+    common_options = ['-t', '-A']
+    if ports:
+        common_options += _create_forwardopt(ports)
+
+    executessh(hosts, common_options, opt.execcmd)
+
+if __name__=='__main__':
+    main()
