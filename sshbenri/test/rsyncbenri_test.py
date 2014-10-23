78b165845902e5478639e1b9d7cb6a6a53c5fc87
takada-at <takada-at@klab.com>
2014-10-23 20:22:04 +0900

    test

--- sshbenri/test/rsyncbenri_test.py
+++ sshbenri/test/rsyncbenri_test.py
@@ -7,12 +7,12 @@ def test_executersync():
     hosts = ['host']
     srcpath = './'
     destpath = './'
-    cmd = rsyncbenri.createcommand(hosts, srcpath, destpath, dryrun=True)
+    cmd = rsyncbenri.createcommand(hosts, srcpath, destpath)
     expect = "rsync -rv {src} {host}:'{dst}'".format(src=srcpath, dst=destpath, host=hosts[0])
     assert expect == cmd
 
     hosts = ['gw', 'host']
-    cmd = rsyncbenri.createcommand(hosts, srcpath, destpath, dryrun=True)
+    cmd = rsyncbenri.createcommand(hosts, srcpath, destpath)
     expect = "rsync -rv -e 'ssh -t -A gw ssh' {src} {host}:'{dst}'".format(src=srcpath, dst=destpath, host=hosts[-1])
     assert expect == cmd
 
